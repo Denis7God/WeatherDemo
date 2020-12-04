c1b5b1e621d022241284779bd1a68e751bbb0652
Denis Godovaniuk <denis7god@gmail.com>
2020-12-04 17:51:28 +0200

    Сделан контроллер с таблицей-перечнем городов и текущей погодой в них.
    Добавлен первый контроллер, содержащий TableView с перечнем городов и TableCells с информацией о температуре.
    Для каждого элемента таблицы происходит запрос к OpenWeather API и нужная информация отображается для каждого города - название, температура, иконка с погодными условиями.

---
+++ WeatherDemo/CityTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CityWeatherTableViewCell.swift
+//  WeatherDemo
+//
+//  Created by Denis Godovaniuk on 03.12.2020.
+//
+
+import UIKit
+
+class CityTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    @IBOutlet weak var weatherImage: UIImageView!
+    
+    
+}
